2f7d67800e01d22593bd128ca1280d5915e8f030
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-15 15:24:59 +0100

    proj: IEnumerable

---
+++ IEnumerable/IEnumerable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerable
+{
+    class Program
+    {
+        static void Main()
+        {
+            IEnumerable<string> enumerable = new string[] { "A", "B", "C"};
+
+            ///Part 2 Enumerator +
+            IEnumerator<string> enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                string s = enumerator.Current;
+                Console.WriteLine(s);
+            }
+
+            ///Part 1 Only enumerable
+            //foreach (var item in enumerable)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
