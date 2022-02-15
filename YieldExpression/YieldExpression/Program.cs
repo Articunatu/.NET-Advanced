b0a642f028f1c2ac346474058f282ea41e72b124
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-15 15:23:59 +0100

    proj: Yield

---
+++ YieldExpression/YieldExpression/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldExpression
+{
+    class Program
+    {
+
+        static List<int> intList = new List<int>();
+
+        public static void FillV()
+        {
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            intList.Add(5);
+            intList.Add(6);
+            intList.Add(7);
+        }
+        static void Main(string[] args)
+        {
+            FillV();
+            foreach (var item in FilterWithYeild())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        ///Creates a separate list
+        static IEnumerable<int> Filter()
+        {
+            List<int> Temp = new List<int>();
+            foreach (var i in intList)
+            {
+                if (i > 4)
+                {
+                    Temp.Add(i);
+                }
+            }
+            return Temp;
+        }
+
+        ///
+        static IEnumerable<int> FilterWithYeild()
+        {
+            foreach (var I in intList)
+            {
+                if (I > 4)
+                {
+                    yield return I;
+                }
+            }
+        }
+    }
+}
---
+++ YieldExpression/YieldExpression/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace YieldExpression
+{
+    class User : IEnumerable<bool>
+    {
+        public IEnumerator<bool> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
