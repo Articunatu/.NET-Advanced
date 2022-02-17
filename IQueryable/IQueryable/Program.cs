a4c9ab06041dca5400ca4755ae3dad741fbd213b
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-02-17 09:32:23 +0100

    proj: IEquatable

---
+++ IQueryable/IQueryable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IQueryable/IQueryable/StudentEnumerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQueryable
+{
+    public class StudentEnumerator : IEnumerator<Student>
+    {
+        private int  { get; set; }
+        private int curent { get; set; }
+        private int effort { get; set; }
+
+        public StudentEnumerator()
+        {
+
+        }
+    }
+}
