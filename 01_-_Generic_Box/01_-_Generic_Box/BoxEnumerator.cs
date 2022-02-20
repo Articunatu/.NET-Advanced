d3764c86ae61dfae613f79dc33b3dacf0176e4ee
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-20 20:19:18 +0100

    feat: Manual iterator class which enables the collection to count, have index, etc.

---
+++ 01_-_Generic_Box/01_-_Generic_Box/BoxEnumerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01___Generic_Box
+{
+    class BoxEnumerator : IEnumerator<Box>
+    {
+        public BoxCollection boxes;
+        public int curIndex;
+        public Box curBox;
+        public int Count
+        {
+            get
+            {
+                return boxes.Count;
+            }
+        }
+
+        public BoxEnumerator(BoxCollection _boxes)
+        {
+            boxes = _boxes;
+            curIndex = -1;
+            curBox = default;
+        }
+
+        public Box Current { get { return curBox; } }
+
+        object IEnumerator.Current { get { return curIndex; } }
+
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            if (++curIndex >= boxes.Count())
+            {
+                return false;
+            }
+            else
+            {
+                curBox = boxes[curIndex];
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
