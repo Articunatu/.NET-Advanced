f6db6a07364830e7c0ab02a4ed52a2f346733646
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-20 20:16:24 +0100

    feat: Main class with properties and overriden methods

---
+++ 01_-_Generic_Box/01_-_Generic_Box/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _01___Generic_Box
+{
+    public class Box : IEquatable<Box>
+    {
+        public Box(int _height, int _base, int _width)
+        {
+            Height = _height;
+            Base = _base;
+            Length = _width;
+        }
+
+        public int Height { get; set; }
+        public int Base { get; set; }
+        public int Length { get; set; }
+
+
+        public bool Equals([AllowNull] Box other)
+        {
+            if (new SameBoxDimensions().Equals(other, this))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool EqualsVol([AllowNull] Box other)
+        {
+            if (new SameBoxVolume().Equals(other, this))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
