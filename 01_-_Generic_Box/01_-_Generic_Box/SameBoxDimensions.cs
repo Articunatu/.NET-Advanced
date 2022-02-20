bce8210a0d209d4258b06536ff075a573cd79f2e
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-20 20:19:49 +0100

    feat: Two classes for comparitve methods, which overrides the main class

---
+++ 01_-_Generic_Box/01_-_Generic_Box/SameBoxDimensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _01___Generic_Box
+{
+    public class SameBoxDimensions : EqualityComparer<Box>
+    {
+        public override bool Equals([AllowNull] Box x, [AllowNull] Box y)
+        {
+            if (x.Base == y.Base && x.Height == y.Height && x.Length == y.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode([DisallowNull] Box obj)
+        {
+            var hCode = obj.Base ^ obj.Height ^ obj.Length;
+            Console.WriteLine("HC: {0}", hCode.GetHashCode());
+            return hCode.GetHashCode();
+        }
+    }
+}
---
+++ 01_-_Generic_Box/01_-_Generic_Box/SameBoxVolume.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _01___Generic_Box
+{
+    public class SameBoxVolume : EqualityComparer<Box>
+    {
+        public override bool Equals([AllowNull] Box x, [AllowNull] Box y)
+        {
+            if (x.Base * x.Height * x.Length == y.Base * y.Height * y.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode([DisallowNull] Box obj)
+        {
+            var hCode = obj.Base ^ obj.Height ^ obj.Length;
+            return hCode.GetHashCode();
+        }
+    }
+}
