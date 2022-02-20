ade3bc493d8e7566e41ae2bf68edfe006a0b221b
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-20 20:20:35 +0100

    feat: Creation and display of objects, confirms bool working

--- 01_-_Generic_Box/01_-_Generic_Box/Program.cs
+++ 01_-_Generic_Box/01_-_Generic_Box/Program.cs
@@ -4,9 +4,47 @@ namespace _01___Generic_Box
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
+            BoxCollection boxes = new BoxCollection();
+            Box box = new Box(4,5,6);
+            boxes.Add(box);
+            box = new Box(7, 3, 2);
+            boxes.Add(box);
+            box = new Box(1, 42, 1);
+            boxes.Add(box);
+            boxes.Display();
+
+            box = new Box(4, 5, 6);
+            boxes.Add(box);
+            boxes.Remove(boxes[0]);
+            boxes.Display();
+
+            try
+            {
+                if (boxes.Contains(boxes[2]))
+                {
+                    Console.WriteLine("Ja, angiven låda finns!");
+                }
+                else
+                {
+                    Console.WriteLine("Nej, den lådan finns inte....");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Nej, den lådan finns inte...."); 
+            }
+            
+
+            if (boxes[0].EqualsVol(boxes[1]))
+            {
+                Console.WriteLine("Ja, lådorna har samma volym");
+            }
+            else
+            {
+                Console.WriteLine("Nej, lådorna har INTE samma volym");
+            }
+        }        
     }
 }
