b73c05fe2769098ac25069ccfb5b16a49d9f560c
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-02-28 14:20:49 +0100

    doc: Planning

--- 02_-_Linq/02_-_Linq/Program.cs
+++ 02_-_Linq/02_-_Linq/Program.cs
@@ -8,8 +8,9 @@ namespace _02___Linq
         static void Main()
         {
            ///Skapa ett nytt projekt i VS och skapa Fyra modeller, Lärare, Student, Kurs(exempel :SUT21), Ämne(Avancerad.net).
-           ///Anpassa relationen mellan tabellerna!till exempel(en lärare har en eller flera ämnen)
-           ///Bygga DB till modeller och fylla på data i Alla tabeller.
+           ///Anpassa relationen mellan tabellerna!till exempel(en lärare har en eller flera ämnen) 
+           /// - Två kopplingtabeller i script-form till Ämnen: från Elever och Lärare
+           ///Bygga DB till modeller och fylla på data i Alla tabeller. (EF DB först)
            ///Hämta alla lärare som undervisa matte
            ///Hämta alla elever med sina lärare.
            ///Kolla om ämnen tabell Contains programmering1 eller inte.
