30ed7203f05b057baa07566437c9267043f797fa
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-03-09 10:13:46 +0100

    proj: 3rd assignment with threads

---
+++ 03_-_Threads_Asynchronization/03_-_Threads_Asynchronization/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03___Threads_Asynchronization
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        
+        private double velocity = 120/3.6; ///120 km/h
+        public double Velocity { get => velocity; set => velocity = value; }
+
+        private int currentDistance = 0;
+        public bool NoFuel { get; set; }
+        public bool Punctation { get; set; }
+        public bool BirdOnWindow { get; set; }
+        public bool MotorMalfuntion { get; set; }
+        public int CurrentDistance { get => currentDistance; set => currentDistance = value; }
+
+        public void CheckOccurance(Car car)
+        {
+
+        }
+    }
+}
---
+++ 03_-_Threads_Asynchronization/03_-_Threads_Asynchronization/Competition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace _03___Threads_Asynchronization
+{
+    class Competition
+    {
+        static Car car1 = new Car { Name = "Pegeuot" };
+        static Car car2 = new Car { Name = "Toyota" };
+
+        static readonly int distance = 10000; ///10 km
+        static void Main()
+        {
+            Thread thread1 = new Thread(FirstDrive);
+            Thread thread2 = new Thread(SecondDrive);
+        }
+
+        static void FirstDrive()
+        {
+            for (double i = 0; i < distance; i++)
+            {
+                if (i % car1.Velocity == 0)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void SecondDrive()
+        {
+            for (double i = 0; i < distance; i++)
+            {
+                if (i % car1.Velocity == 0)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void CheckProblem()
+        {
+
+        }
+    }
+}
