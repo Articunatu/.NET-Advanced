ad7fab11adbc696fa14ece5ca4785c3f94d33980
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-03-14 13:40:16 +0100

    proj: Final tutorial for tasks

---
+++ Async_Tutorial/Async_Tutorial/AsyncAwait.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Tutorial
+{
+    static class AsyncAwait
+    {
+        static void Main()
+        {
+            Task task = new Task(CallMethod);
+            task.Start();
+            task.Wait();
+
+            string FilePath = @"C:\Users\chris\OneDrive\Skrivbord\toread.txt";
+            string[] lines = File.ReadAllLines(FilePath);
+            List<string> LI = new List<string>();
+            LI = File.ReadAllLines(FilePath).ToList();
+            foreach (var item in LI)
+            {
+                Console.WriteLine(item);
+            }
+
+            LI.Add("Hej från dej");
+            File.WriteAllLines(FilePath, LI);
+        }
+
+        static async void CallMethod()
+        {
+            string filePath = "C:\\Users\\chris\\OneDrive\\Skrivbord\toread.txt";
+            Task<int> t = ReadFile(filePath);
+
+            Console.WriteLine("Work 1.......Started ");
+            Console.WriteLine("Work 2.......Started ");
+            int l = await t;
+            Console.WriteLine("Total Length " + 1);
+
+            Console.WriteLine("Work 1 ..... Ended");
+            Console.WriteLine("Work 2 ..... Ended");
+        }
+
+        static async Task<int> ReadFile(string file)
+        {
+            int Length = 0;
+            Console.WriteLine("File reading is start ....");
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string s = await reader.ReadToEndAsync();
+                Length = s.Length;
+            }
+            Console.WriteLine("File reading is ended");
+            return Length;
+        }
+    }
+}
---
+++ Async_Tutorial/Async_Tutorial/Program.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Threading.Tasks;
+
+//namespace Async_Tutorial
+//{
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Mehto1d();
+//            //Method2();
+
+//            CallMethod();
+//            Console.ReadKey();
+//        }
+
+//        public static async void CallMethod()
+//        {
+//            Task<int> task = Mehto1d();
+//            Method2();
+//            int count = await task;
+//            Method3(count);
+
+//        }
+//        public static async Task<int> Mehto1d()
+//        {
+//            int Count = 0;
+//            await Task.Run(() =>
+//            {
+//                for (int i = 0; i < 5; i++)
+//                {
+//                    Console.WriteLine("Method 1");
+//                    Task.Delay(1000).Wait();
+
+//                }
+//            });
+//            return Count;
+//        }
+
+//        public static void Method2()
+//        {
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine("Method 2");
+//                Task.Delay(1000).Wait();
+
+//            }
+//        }
+
+//        public static void Method3(int count)
+//        {
+//            Console.WriteLine("Total Count is : " + count);
+//        }
+//    }
+//}
