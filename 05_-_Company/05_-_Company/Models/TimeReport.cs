2d3b70146ea1ea48b3d8b594376fc299e2d20a23
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-03-29 11:16:58 +0200

    proj: Final project of Advanced .NET Course

---
+++ 05_-_Company/05_-_Company/Models/TimeReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _05___Company_DB.Models
+{
+    public class TimeReport
+    {
+        [Key]
+        public int? TR_ID { get; set; }
+        public int? EmployeeID { get; set; }
+        public int? ProjectID { get; set; }
+        public int Hours { get; set; }
+        public DateTime WeekDate { get; set; }
+    }
+}
