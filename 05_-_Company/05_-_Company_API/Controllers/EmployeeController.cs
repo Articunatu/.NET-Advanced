b1f81a00972684ab8db7971acd18d789a19fc680
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-03-30 12:31:30 +0200

    feat: Controller for Employee

--- 05_-_Company/05_-_Company_API/Controllers/EmployeeController.cs
+++ 05_-_Company/05_-_Company_API/Controllers/EmployeeController.cs
@@ -13,16 +13,137 @@ namespace _05___Company_API.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        private ICompany<Employee> company;
-        public EmployeeController()
+        private IEmployee _iemployee;
+        public EmployeeController(IEmployee iemployee)
         {
-
+            _iemployee = iemployee;
         }
 
-        [HttpGet("week{week}/emp{eid}")]
-        public void ReadHours(int week, int eid)
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee newEmployee)
         {
+            try
+            {
+                if (newEmployee == null)
+                {
+                    return BadRequest();
+                }
+                var createdProduct = await _iemployee.Create(newEmployee);
+                return CreatedAtAction(nameof(ReadEmployee), new { id = createdProduct.EmployeeID }, createdProduct);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error to create new employee to database.....");
+            }
+        }
 
+        [HttpGet("e{id}")]
+        public async Task<ActionResult<Employee>> ReadEmployee(int id)
+        {
+            try
+            {
+                var readEmployee = await _iemployee.Read(id);
+                if (readEmployee == null)
+                {
+                    return NotFound();
+                }
+                return readEmployee;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error to retrieve the chosen employee from database.....");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Employee>> ReadAllEmployees()
+        {
+            try
+            {
+                return Ok(await _iemployee.ReadAll());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error to retrieve employees from database.....");
+            }
+        }
+
+        [HttpPut("e{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            try
+            {
+                if (id != employee.EmployeeID)
+                {
+                    return BadRequest("Employee ID Doesn't Exist.....");
+                }
+                var uptEmployee = await _iemployee.Read(id);
+                if (uptEmployee == null)
+                {
+                    return NotFound($"Emplyee with ID {id} Not Found........");
+                }
+                return await _iemployee.Update(uptEmployee, id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error to update employee to database.....");
+            }
+        }
+
+        [HttpDelete("e{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            try
+            {
+                var delEmployee = await _iemployee.Read(id);
+                if (delEmployee == null)
+                {
+                    return NotFound($"Employee with ID {id} Not Found..........");
+                }
+                return await _iemployee.Delete(delEmployee);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error to delete employee from database.....");
+            }
+        }
+
+        [HttpGet("p{projectId}")]
+        public async Task<ActionResult<Employee>> ReadProjectsEmployees(int projectId)
+        {
+            try
+            {
+                return Ok(await _iemployee.ProjectsEmployees(projectId));
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error to retrieve employees of this project from database.....");
+            }
+        }
+        
+        [HttpGet("week{week}/emp{eid}")]
+        public async Task<ActionResult<object>> ReadHours(int week, int eid)
+        {
+            try
+            {
+                var employeesHours = await _iemployee.WeeklyHours(week, eid);
+                if (employeesHours == null)
+                {
+                    return NotFound();
+                }
+                return employeesHours;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error to retrieve the chosen week and employee from database.....");
+            }
         }
     }
 }
