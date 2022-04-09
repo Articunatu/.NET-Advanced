1652c278b352b1805df70f32578e2c0c5011415b
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-04-09 10:37:50 +0200

    feat: Error message for minimum lengths

--- 05_-_Company/05_-_Company/Models/Employee.cs
+++ 05_-_Company/05_-_Company/Models/Employee.cs
@@ -8,10 +8,12 @@ namespace _05___Company_DB.Models
         [Key]
         public int EmployeeID { get; set; }
         [Required(ErrorMessage = "Anställdas förnamn måste vara med!")]
-        [StringLength(22, MinimumLength = 1, ErrorMessage = "Ett förnamn kan ej vara längre än 22 bokstäver långt!")]
+        [StringLength(22, ErrorMessage = "Ett förnamn kan ej vara längre än 22 bokstäver långt!")]
+        [MinLength(1, ErrorMessage = "Ett förnamn måste vara minst en bokstav långt!")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Anställdas efternamn måste vara med!")] 
-        [StringLength(22, MinimumLength = 2, ErrorMessage = "Ett efternamn kan ej vara längre än 22 bokstäver långt!")]
+        [StringLength(22, ErrorMessage = "Ett efternamn kan ej vara längre än 22 bokstäver långt!")]
+        [MinLength(2, ErrorMessage = "Ett efternamn kan ej vara kortare än 2 bokstäver långt!")]
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
--- 05_-_Company/05_-_Company/Models/Project.cs
+++ 05_-_Company/05_-_Company/Models/Project.cs
@@ -7,6 +7,7 @@ namespace _05___Company_DB.Models
         [Key]
         public int ProjectID { get; set; }
         [Required(ErrorMessage = "Ett projekt måste ha en titel!"), ]
+        [StringLength(20, ErrorMessage = "Ett projekts titel kan ej vara längre än 22 bokstäver långt!")]
         public string Title { get; set; }
         public string Description { get; set; }
     }
